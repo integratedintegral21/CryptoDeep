098474689ed2fa9f13ced1d57cd183db4fb10125
wojciech <w.boncela@gmail.com>
2022-08-15 17:30:46 +0200

    new data

--- NN/inference.py
+++ NN/inference.py
@@ -3,6 +3,7 @@ from train import load_data, get_normalized_sequences, create_model
 import matplotlib.pyplot as plt
 import numpy as np
 import joblib
+from sklearn.metrics import mean_absolute_error, mean_squared_log_error
 
 
 def main():
@@ -18,17 +19,25 @@ def main():
     model.evaluate(X_val, y_val)
 
     y_pred = model.predict(X_val)[:, 0]
+    unscaled_inference = []
     for t_stp, x, y_t, y_p in zip(timestamps, X_val, y_val, y_pred):
         # Inverse scaling
         x = x * np.sqrt(std_scaler.var_) + std_scaler.mean_
         y_t = y_t * np.sqrt(std_scaler.var_[-1]) + std_scaler.mean_[-1]
         y_p = y_p * np.sqrt(std_scaler.var_[-1]) + std_scaler.mean_[-1]
+        unscaled_inference.append([y_t, y_p])
         openings = x[:, 0]
         closings = x[:, -1]
         plt.plot(t_stp, closings)
         plt.scatter(t_stp[-1:], [y_t])
         plt.scatter(t_stp[-1:], [y_p], c='red')
         plt.show()
+    unscaled_inference = np.asarray(unscaled_inference)
+    print("Mean absolute prediction error (unscaled predictions):", mean_absolute_error(unscaled_inference[:, 0],
+                                                                                        unscaled_inference[:, 1]))
+    print("Mean squared logarithmic prediction error (unscaled predictions):",
+          mean_squared_log_error(unscaled_inference[:, 0],
+                                 unscaled_inference[:, 1]))
 
 
 if __name__ == "__main__":
