79b8147f4f298ce95891389f19daa943d0ff09a4
wojciechboncela <w.boncela@gmail.com>
2022-08-21 12:59:15 +0200

    Requirements updated

--- logic/scraper/run_scraper.py
+++ logic/scraper/run_scraper.py
@@ -1,21 +1,38 @@
 import logging
 import os.path
 import asyncio
+from configparser import ConfigParser
 
 from web_scraper import CryptodatadownloadScraperDB
 
 
-async def update_scraper():
-    scraper = CryptodatadownloadScraperDB('ETH', 'GBP', 'localhost', 'cryptodb', 'cryptodb', 'cryptodb',
-                                          os.path.dirname(__file__) + '/cache')
+def config(filename=os.path.dirname(__file__) + '/db_config/database.ini', section='postgresql'):
+    parser = ConfigParser()
+    parser.read(filename)
+
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
+
+
+async def update_scraper(db_params):
+    scraper = CryptodatadownloadScraperDB('ETH', 'GBP', db_params['host'], db_params['database'], db_params['user'],
+                                          db_params['password'], os.path.dirname(__file__) + '/cache')
     scraper.update_db()
     await asyncio.sleep(3600)
 
 
 async def main():
     logging.basicConfig(level=logging.INFO)
+    db_params = config()
     while True:
-        await update_scraper()
+        await update_scraper(db_params)
 
 
 if __name__ == "__main__":
