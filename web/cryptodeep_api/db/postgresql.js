71d7fb4dcbc830620c1bf3a087bb9edcee07b472
wojciechboncela <w.boncela@gmail.com>
2022-08-23 17:03:16 +0200

    database integration with the api

--- web/cryptodeep_api/db/postgresql.js
+++ web/cryptodeep_api/db/postgresql.js
@@ -8,10 +8,28 @@ const PASSWORD = 'cryptodb'
 const pgp = require('pg-promise')(/* options */)
 const db = pgp('postgres://' + USER + ':' + PASSWORD + '@' + HOST + ':' + PORT + '/' + DB)
 
+const convertToPostgresqlFormat = (date) => {
+    return date.toISOString().replace('T', ' ').replace('Z', '')
+}
+
 async function get_all_records(crypto, currency) {
     const table_name = crypto + '_' + currency + '_records'
-    const records = await db.many('SELECT * FROM ' + table_name)
-    return records
+    return await db.many('SELECT * FROM ' + table_name)
+}
+
+async function get_n_latest_records(crypto, currency, n_records) {
+    const table_name = crypto + '_' + currency + '_records'
+    const sql = 'SELECT * FROM ' + table_name + ' ORDER BY timestamp DESC LIMIT '
+        + n_records
+    return await db.many(sql)
+}
+
+async function get_records_between(crypto, currency, start_date, end_date) {
+    const table_name = crypto + '_' + currency + '_records'
+    const sql = 'SELECT * FROM ' + table_name + ' WHERE timestamp BETWEEN $1 AND $2'
+    return await db.many(sql, [convertToPostgresqlFormat(start_date), convertToPostgresqlFormat(end_date)])
 }
 
 module.exports.get_all_records = get_all_records
+module.exports.get_n_latest_records = get_n_latest_records
+module.exports.get_records_between = get_records_between
--- web/cryptodeep_api/routes/crypto.js
+++ web/cryptodeep_api/routes/crypto.js
@@ -1,18 +1,81 @@
+var moment = require('moment');
 var express = require('express');
 var router = express.Router();
 var db = require('../db/postgresql')
+
+function createDateAsUTC(date) {
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(),
+        date.getSeconds()));
+}
+
+router.get('/:cryptoSymbol/:currencySymbol/all', async function (req,
+                                                                 res,
+                                                                 next){
+    try {
+        const records = (await db.get_all_records(req.params.cryptoSymbol, req.params.currencySymbol))
+        records.map((element) => {
+            delete element.record_id
+            element.timestamp = createDateAsUTC(element.timestamp).toISOString()
+            return element
+        })
+        res.send(
+            records.sort((a, b) => {
+                if (a.timestamp < b.timestamp) {
+                    return 1
+                }
+                return -1
+            })
+        )
+    } catch (error) {
+        next(error)
+    }
+})
 
 router.get('/:cryptoSymbol/:currencySymbol/:n_records', async function (req,
                                                                  res,
                                                                  next){
     try {
-        const records = await db.get_all_records(req.params.cryptoSymbol, req.params.currencySymbol)
-        res.send(records.sort((a, b) => {
-            if (a.timestamp < b.timestamp) {
+        const records = (await
+            db.get_n_latest_records(req.params.cryptoSymbol, req.params.currencySymbol, req.params.n_records))
+        records.map((element) => {
+            delete element.record_id
+            element.timestamp = createDateAsUTC(element.timestamp).toISOString()
+            return element
+        })
+        res.send(
+            records.sort((a, b) => {
+                if (a.timestamp < b.timestamp) {
+                    return 1
+                }
                 return -1
-            }
-            return 1
-        }))
+            })
+        )
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get('/:cryptoSymbol/:currencySymbol/:start_date/:end_date', async function (req,
+                                                                 res,
+                                                                 next){
+    try {
+        const start_date = createDateAsUTC(moment(req.params.start_date, "YYYYMMDDhhmmss").toDate())
+        const end_date = createDateAsUTC(moment(req.params.end_date, "YYYYMMDDhhmmss").toDate())
+        const records = (await
+            db.get_records_between(req.params.cryptoSymbol, req.params.currencySymbol, start_date, end_date))
+        records.map((element) => {
+            delete element.record_id
+            element.timestamp = createDateAsUTC(element.timestamp).toISOString()
+            return element
+        })
+        res.send(
+            records.sort((a, b) => {
+                if (a.timestamp < b.timestamp) {
+                    return 1
+                }
+                return -1
+            })
+        )
     } catch (error) {
         next(error)
     }
